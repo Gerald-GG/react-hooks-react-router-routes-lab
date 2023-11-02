13c5636658c4671a4111c405cc2a35093bbb4a9b
Gerald Gicharu <gicharugerald@gmail.com>
2023-11-02 14:21:35 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,20 @@ import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="actors" component={Actors} />
+      </div>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
